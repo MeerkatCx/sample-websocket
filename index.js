9f4f9ba8a63003414a94e5e17067fd1f80bf4615
Christian Quispe H <christian.quispeh@gmail.com>
2024-10-18 14:42:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,24 +10,24 @@ const app = express();
 app.use(express.static(path.join(__dirname, '/public')));
 
 const server = createServer(app);
-const wss = new WebSocket.Server({ server });
+const wss = new WebSocket.Server({ server, path: '/message' });
 
 wss.on('connection', function (ws) {
   const id = setInterval(function () {
     ws.send(JSON.stringify(process.memoryUsage()), function () {
       //
-      // Ignore errors.
+      // Ignorando errores.
       //
     });
   }, 100);
-  console.log('started client interval');
+  console.log('Cliente conectado, iniciando intervalo');
 
   ws.on('close', function () {
-    console.log('stopping client interval');
+    console.log('Deteniendo intervalo del cliente');
     clearInterval(id);
   });
 });
 
 server.listen(8080, function () {
-  console.log('Listening on http://0.0.0.0:8080');
+  console.log('Escuchando en http://0.0.0.0:8080');
 });
